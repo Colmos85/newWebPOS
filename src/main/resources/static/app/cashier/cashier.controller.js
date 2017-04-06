c901ce7e6f5ef7748867254080f0083e9652a7d3
Unknown <colm.osullivan4@mycit.ie>
2017-04-06 01:42:24 +0100

    fixed sales view.. print not working, not in same order and layout

--- src/main/resources/static/app/cashier/cashier.controller.js
+++ src/main/resources/static/app/cashier/cashier.controller.js
@@ -235,7 +235,7 @@
             //popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + printContents + '</body></html>');
             //popupWin.document.close();
             //$mdDialog.hide();
-            $scope.showSalesSummary = false; 
+            //$scope.showSalesSummary = false; 
         };
 
         $scope.makeTransaction = function(change){
